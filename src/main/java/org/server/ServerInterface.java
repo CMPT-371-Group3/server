a8e51b82eba4c70b1d6d5205a591ec431150ccd1
shabbiryusufali <shabyusufali@gmail.com>
2023-07-14 00:54:51 -0700

    Address Comments

--- src/main/java/org/server/ServerInterface.java
+++ src/main/java/org/server/ServerInterface.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 public interface ServerInterface {
     ArrayList<Client> GetClients();
     ServerSocket GetServerSocket();
-    public void AddClient(Client client);
-    public Status DisconnectClient(String ipAddress);
-    public String ListenForMessage();
-    public Socket ListenServer();
+    void AddClient(Client client);
+    Status DisconnectClient(String ipAddress);
+    String ListenForMessage();
+    Socket ListenServer();
 
 }
