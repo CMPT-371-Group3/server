3eb1e8303a8c74baa784e21eb2d2e8a3b2127ea4
shabbiryusufali <shabyusufali@gmail.com>
2023-07-14 00:55:05 -0700

    Add Potential Statuses We Will Need

--- src/main/java/org/server/Status.java
+++ src/main/java/org/server/Status.java
@@ -3,5 +3,9 @@ package org.server;
 public enum Status {
     SUCCESS,
     FAILURE,
-    INVALID_ARGUMENT
+    INVALID_ARGUMENT,
+    INVALID_OPERATION,
+    INVALID_MOVE,
+    GAME_WON,
+    GAME_LOST
 }
