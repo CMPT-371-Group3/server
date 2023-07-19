a9a5b61e4b7c7b2bd6814de91dea0462f46e20bc
jeff <jeffreyzhuu@gmail.com>
2023-07-18 20:12:45 -0700

    added some basic broadcasting.. a bit janky

--- src/main/java/org/server/Client.java
+++ src/main/java/org/server/Client.java
@@ -10,31 +10,92 @@ public class Client {
     private Socket socket;
     private PrintWriter output;
     private BufferedReader input;
-
+    private String addressWithPort;
+    private Scanner sc;
+    private Boolean msgSent;
+    private static final Object lock = new Object();
     public static void main(String args[]) {
-        Client client = new Client("localhost", 6000);
-        String line = null;
-        Scanner sc = new Scanner(System.in);
-        System.out.print(client.getIpAddress() + ":" + client.getPortNumber() + ": ");
-        line =  sc.nextLine();
-        while (!line.equalsIgnoreCase("exit")) {
-            client.getOutput().println(line);
-            client.getOutput().flush();
-            String response = null;
-            try {
-                response = client.getInput().readLine();            
-            } catch(IOException e) {
-                e.printStackTrace();
-            }
-            if (response != null) {
-                System.out.println("Server: " + response);
-            }
-            line = "";
-            System.out.print(client.getIpAddress() + ":" + client.getPortNumber() + ": " + line);
+        try {
+            Client client = new Client("localhost", 6000);
+            String address = client.getInput().readLine();
+            client.setAddressWithPort(address);
+            String line = null;
+            Scanner sc = new Scanner(System.in);
+
+            // Separate thread to read messages from server
+            new Thread(() -> {
+                try {
+                    String serverMessage;
+                    while((serverMessage = client.getInput().readLine()) != null) {
+                        switch (serverMessage) {
+                            case "EXIT":
+                                return;
+                            case "MESSAGE":
+                                System.out.println("\nIncoming: MESSAGE");
+                                String message = client.getInput().readLine();
+                                System.out.print(message);
+                                // Scanner scanner = new Scanner(System.in);
+                                message = client.getMessage();
+                                System.out.println("message: " + message + " length: " + message.length());
+                                client.getOutput().println(message);
+                                client.getOutput().flush();
+                                System.out.println("" + client.getInput().readLine());
+                                synchronized(lock) {
+                                    client.setMsgSent(true);
+                                    lock.notifyAll();
+                                }
+                                break;
+                            case "BROADCAST":
+                                // System.out.println(client.getAddressWithPort() + ": " + serverMessage);
+                                // break;
+                            default:
+                                if (serverMessage.length() > 0)
+                                System.out.println(client.getAddressWithPort() + ": " + serverMessage);
+                        }
+                    }   
+                } catch(IOException e) {
+                    e.printStackTrace();
+                }                  
+            }).start();
+            
+            client.getOutput().println("JOIN");
+            System.out.println(client.getAddressWithPort() + ": ");
+            System.out.println("Select an option:\nEXIT\nMESSAGE");
+            System.out.print("Selection: ");
             line = sc.nextLine();
+            while (!line.equalsIgnoreCase("EXIT")) {
+                switch (line) {
+                    case "MESSAGE":
+                        client.getOutput().println("MESSAGE");
+                        client.getOutput().flush();
+                        break;
+                    default:
+                }
+                synchronized (lock) {
+                    try {
+                        lock.wait(); // Wait for notification from the other thread
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                client.setMsgSent(false);
+                System.out.println("\nSelect an option:\nEXIT\nMESSAGE");
+                System.out.print("Selection: ");                
+                line = sc.nextLine();              
+            }
+            if (line.equalsIgnoreCase("exit")) {
+                client.getOutput().println("EXIT");
+                client.getOutput().flush();                    
+            } 
+            client.disconnect();
+            /*
+            */
+        } catch(IOException e) {
+            e.printStackTrace();
         }
-        client.disconnect();
     }
+
+
 
     public Client(String ipAddress, int portNumber) {
         try {
@@ -46,10 +107,18 @@ public class Client {
             OutputStream outputStream = socket.getOutputStream();
             this.output = new PrintWriter(outputStream, true);
             this.input = new BufferedReader(new InputStreamReader(inputStream));
+            this.addressWithPort = "";
+            this.sc = new Scanner(System.in);
+            this.msgSent = false;
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
         }
     }
+
+    public String getMessage() {
+        return sc.nextLine();
+    }
+
     public String listenForMessage() {
         try {
             // Listen for inputs and then pass them up
@@ -98,6 +167,14 @@ public class Client {
         return this.input;
     }
 
+    public void setAddressWithPort(String address) {
+        this.addressWithPort = address;
+    }
+
+    public String getAddressWithPort() {
+        return this.addressWithPort;
+    }
+
     // @Override
     // public void run() {
     //     try {
@@ -110,4 +187,48 @@ public class Client {
     //         System.out.println("Error: " + e.getMessage());
     //     }
     // }
+
+    public void setMsgSent(Boolean b) {
+        this.msgSent = b;
+    }
+
+    public Boolean getMsgSent() {
+        return this.msgSent;
+    }
+
+    public class ClientThread implements Runnable {
+
+        private Client client;
+
+        public ClientThread(Client c) {
+            this.client = c;
+        }
+
+        public void run() {
+            try {
+                String serverMessage;
+                while((serverMessage = client.getInput().readLine()) != null) {
+                    switch (serverMessage) {
+                        case "EXIT":
+                            return;
+                        case "MESSAGE":
+                            System.out.println("\nIncoming: MESSAGE");
+                            String message = client.getInput().readLine();
+                            System.out.print(message);
+                            // Scanner scanner = new Scanner(System.in);
+                            message = sc.nextLine();
+                            System.out.println("message: " + message + " length: " + message.length());
+                            client.getOutput().println(message);
+                            client.getOutput().flush();
+                            break;
+                        default:
+                    }
+                    System.out.println("\nSelect an option:\nEXIT\nMESSAGE");
+                    System.out.print("Selection: ");
+                }   
+            } catch(IOException e) {
+                e.printStackTrace();
+            }            
+        }
+    }
 }
